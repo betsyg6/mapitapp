d2e0f3f42c76c99b64fa9f662f33672a7d991268
Betsy Groton <grotoned@gmail.com>
2020-10-26 16:55:28 -0400

    fix: travis yaml

--- index.js
+++ index.js
@@ -73,6 +73,8 @@ async () => {
 // 	});
 // });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 });
+
+module.exports = server;
---
+++ test/index.spec.js
@@ -0,0 +1,10 @@
+/** @format */
+
+const expect = require('chai').expect;
+const server = require('../index');
+
+describe('test', () => {
+	it('should return a string', () => {
+		expect('hi').to.equal('hi');
+	});
+});
